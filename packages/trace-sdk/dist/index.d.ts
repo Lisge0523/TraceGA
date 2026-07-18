34eca70e7f8ee3d2d56e58b78c8d7ab3603166c4
yhanli4806-coder <yhanli4806@gmail.com>
2026-07-19 00:10:49 +0800

    chore: 移除误提交的dist打包产物

--- packages/trace-sdk/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { Reporter } from './reporter/index';
-export type { TrackEventData, TraceConfig, EnvInfo, CommonParams, ITraceCore } from './types';
-export type { Priority } from './core/PriorityScheduler';
-export declare const reporterVersion = "0.0.1";
-export declare const pluginVersion = "0.0.1";
-export declare const utilsVersion = "0.0.1";
